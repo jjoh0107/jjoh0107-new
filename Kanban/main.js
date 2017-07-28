8577761a7f7a62902ca5f4be8b841a2c86cd250a
jjoh0107 <jojh0107@naver.com>
2017-07-28 15:33:47 +0900

    delete

--- Kanban/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jihye on 2017-07-25.
- */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-
-ReactDOM.render( <App />, document.getElementById('app'));
